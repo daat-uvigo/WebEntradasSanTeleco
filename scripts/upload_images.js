2f75b303a3b7a7c464d90aa0d8880efe8f7d2596
PiterWeb <piterzdev@gmail.com>
2026-08-03 22:06:10 +0200

    Mejora de script imagenes (ahora convierte a webp antes de subir pero tarda casi un segundo por cada imagen a subir)

--- scripts/upload_images.js
+++ scripts/upload_images.js
@@ -1,3 +1,4 @@
+import sharp from "sharp"
 import fs from "fs/promises"
 import path from "path"
 import PocketBase from "pocketbase"
@@ -5,6 +6,9 @@ import PocketBase from "pocketbase"
 export const baseUrl = "https://santeleco.uvigo.es/api"
 export const pb = new PocketBase(baseUrl)
 
+/**
+ * @param {string} dir 
+ */
 async function* walk(dir) {
     for await (const d of await fs.opendir(dir)) {
         const entry = path.join(dir, d.name);
@@ -16,10 +20,15 @@ async function* walk(dir) {
 const photos_paths = walk("./fotos_santeleco/")
 
 for await (const photo_path of photos_paths) { 
-  const filename = path.basename(photo_path)
+  const ext = path.extname(photo_path)
+  const filename_without_ext = path.basename(photo_path, ext)
+
   const content = await fs.readFile(photo_path)
+  const webp_content = await sharp(content).webp().toBuffer()
+      
   const new_p = await pb.collection('photos').create({
-    "img" : new File([content], filename)
+    "img" : new File([webp_content], `${filename_without_ext}.webp`)
   });
+
   console.log(new_p)
 }
