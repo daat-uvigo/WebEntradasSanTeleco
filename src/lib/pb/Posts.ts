6f284ee32cc0713d160c138557babe35837ca41b
PiterWeb <piterzdev@gmail.com>
2026-08-31 17:19:08 +0200

    Posts info mejoras

--- src/lib/pb/Posts.ts
+++ src/lib/pb/Posts.ts
@@ -10,7 +10,7 @@ const postsColl = "posts"
 
 export async function getPosts(): Promise<Post[]> {
   const list = await pb.collection(postsColl).getFullList()
-  return list.map(i => {
+  return list.toReversed().map(i => {
     return {id: i.id, img: pb.files.getURL(i, i.img), url: i.url}
   })
 }
